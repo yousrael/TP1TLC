44693377636a4157c4b0810ad028cbbc4e3dec2a
Your Name <simon.parisse@outlook.fr>
2017-01-20 11:22:13 +0100

    Revert "Implementation board.jsp"
    This reverts commit a3cfdd8fcb7076cf97cda9cd01064eca76dae34b.

--- tp1tlc/appengine-java-guestbook-multiphase/final/src/main/java/com/example/advertisement/Advertisement.java
+++ tp1tlc/appengine-java-guestbook-multiphase/final/src/main/java/com/example/advertisement/Advertisement.java
@@ -2,6 +2,7 @@ package com.example.advertisement;
 
 import java.util.Date;
 
+import com.example.guestbook.Guestbook;
 import com.googlecode.objectify.Key;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
@@ -10,22 +11,14 @@ import com.googlecode.objectify.annotation.Parent;
 
 @Entity
 public class Advertisement {
-	@Parent Key<Board> board;
+	  @Parent Key<Board> board;
 	@Id private int id;
-
-	public String author_email;
-	public String author_id;
+	 public String author_email;
+	  public String author_id;
 	private String title;
-<<<<<<< Updated upstream
 	@Index private double price;
 	@Index private Date date;
 	public Advertisement( String author_email, String author_id, String title, double price, Date date) {
-=======
-	private double price;
-	private Date date;
-
-	public Advertisement(int id, String author_email, String author_id, String title, double price, Date date) {
->>>>>>> Stashed changes
 		super();
 		
 		this.author_email = author_email;
@@ -34,7 +27,6 @@ public class Advertisement {
 		this.price = price;
 		this.date = date;
 	}
-<<<<<<< Updated upstream
 	public Advertisement(String title, double price, Date date) {
 		super();
 		this.title = title;
@@ -44,39 +36,31 @@ public class Advertisement {
 	
 	
 	
-=======
->>>>>>> Stashed changes
 
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public double getPrice() {
 		return price;
 	}
-
 	public void setPrice(double price) {
 		this.price = price;
 	}
-
 	public Date getDate() {
 		return date;
 	}
-
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
+	
 }
