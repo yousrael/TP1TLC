4aa00588af1b95779b2e3ab4712a1a9c52a66d52
yousra <yousraelghzizal@hotmail.fr>
2017-02-09 01:17:46 +0100

    add delete servlet

---
+++ tp1tlc/appengine-java-guestbook-multiphase/final/src/main/java/com/example/advertisement/DeleteServlet.java
@@ -0,0 +1,58 @@
+package com.example.advertisement;
+
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.googlecode.objectify.ObjectifyService;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class DeleteServlet  extends HttpServlet  {
+
+
+	  // Process the http POST of the form
+	  @Override
+	  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	   
+  System.err.println("servlet Delete");
+	      // Find out who the user is.
+	    String boardName=req.getParameter("boardName");
+	   String filter=req.getParameter("filter");
+	  // String delete=req.getParameter("delete");
+	  // System.out.println("boardName: "+boardName+" filter: "+filter+" delete: "+delete);
+	  
+
+		   List<Advertisement> advertisements = ObjectifyService.ofy()
+			          .load()
+			          .type(Advertisement.class) 
+			          .list();
+		   
+		   System.out.println("Database Loded");
+		      
+		   for (Advertisement advertisement : advertisements) {
+			  if(advertisement.title.contains(filter)){
+			   
+					ObjectifyService.ofy().delete().entity(advertisement);
+					System.out.println("remove : "+advertisement.title);
+			   
+			  }
+		   }
+			   
+		   
+	   
+	  
+		   resp.sendRedirect("/board.jsp?boardName=" + boardName+"&filter="+filter);
+
+	//[END all]
+
+	  }
+}
+
