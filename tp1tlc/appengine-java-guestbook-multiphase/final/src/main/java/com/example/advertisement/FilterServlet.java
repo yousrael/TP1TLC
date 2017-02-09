629a632aa175805bea87b902576c284b4580d7f5
yousra <yousraelghzizal@hotmail.fr>
2017-02-09 01:17:37 +0100

    add Search servlet

--- tp1tlc/appengine-java-guestbook-multiphase/final/src/main/java/com/example/advertisement/FilterServlet.java
+++ tp1tlc/appengine-java-guestbook-multiphase/final/src/main/java/com/example/advertisement/FilterServlet.java
@@ -9,6 +9,10 @@ import com.googlecode.objectify.ObjectifyService;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,29 +26,13 @@ public class FilterServlet  extends HttpServlet  {
 	  @Override
 	  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 	   
-
+  System.err.println("servlet Filter");
 	      // Find out who the user is.
 	    String boardName=req.getParameter("boardName");
 	   String filter=req.getParameter("filter");
-	   String delete=req.getParameter("delete");
-	   System.out.println("boardName: "+boardName+" filter: "+filter+" delete: "+delete);
-	   if( delete != null && !delete.isEmpty() && delete.contains("TRUE")){//on demande le delete
-
-		   List<Advertisement> advertisements = ObjectifyService.ofy()
-			          .load()
-			          .type(Advertisement.class) // We want only Advertisements
-			          .list();
-		   for (Advertisement advertisement : advertisements) {
-			   if(filter!=null && !filter.contains("null")&& !filter.contains(""))System.out.println("do \""+advertisement.title+"\"  contain \""+filter+"\" ?");
-			   if(filter==null ||filter.contains("null")||filter.contains("") || advertisement.title.contains(filter)){ // Si tout ou si c'est un advertissement filter
-					ObjectifyService.ofy().delete().entities(advertisement).now();
-					System.out.println("remove : "+advertisement.title);
-			   }
-		   }
-			   
-		   resp.sendRedirect("/board.jsp?boardName=" + boardName);
-	   }
-	   else // on demande une recherche
+	  // String priceMin=req.getParameter("price-min");
+	  // String priceMax=req.getParameter("price-max");
+	   
 		   resp.sendRedirect("/board.jsp?boardName=" + boardName+"&filter="+filter);
 
 	//[END all]
